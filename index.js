b0d7463debaef2e16fceb3855a7b515342dc2de4
ltaylr <ltaylr@protonmail.com>
2024-12-27 18:25:26 -0500

    added character sheet downloading, helmet, dynamic character list view

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+
+app.use(helmet());//set standard security http headers
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -10,7 +13,8 @@ app.use(express.static(__dirname + '/public'));
 let characters = [];
 
 const levelFolders = fs.readdirSync(path.join(__dirname, 'files', 'characters'));
-console.log(levelFolders);
+app.use(express.static(path.join(__dirname, 'files', 'characters')));
+//console.log(levelFolders);
 app.get("/", (req, res, next) => {
     res.render('index', { docTitle: 'Lo Main Dnd'});
 })
@@ -22,8 +26,20 @@ app.get('/characters', (req, res, next) => {
 app.get('/characters/:level', (req, res, next) =>{
     const level = req.params.level;
     fs.readdir(path.join(__dirname, 'files', 'characters', level),(err, chars) =>{
-        res.render('levels', {docTitle: level, characters: chars});
+        res.render('levels', {docTitle: level, characters: chars, level: level});
     });
+})
+
+app.get('/characters/:level/:char', (req, res, next) =>{
+    const level = req.params.level;
+    const char = req.params.char;
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Dispostion', 'attachment; filename="' + char +'"');
+    res.download(path.join(__dirname, 'files', 'characters', level, char));
+    /*,(err, char) =>{
+        res.download(path.join(__dirname, ));
+    });*/
 })
 
 app.get('/campaign', (req, res, next) => {
