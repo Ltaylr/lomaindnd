f10edd5aee97458618f496aa1a506d029ead7849
ltaylr <ltaylr@protonmail.com>
2025-08-16 21:03:11 -0400

    added rate limiter

--- src/config/Session.js
+++ src/config/Session.js
@@ -2,9 +2,18 @@
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const bodyParser = require('body-parser');
+const {rateLimit} = require('express-rate-limit');
 
 module.exports = function(app, configEnv){
+
+    const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+    })
 
     const MONGODB_URI =
         configEnv.dbConnectionString;
@@ -15,7 +24,7 @@ module.exports = function(app, configEnv){
     });
 
     
-
+    app.use(limiter);
     app.use(
         session({
         secret: configEnv.secrets,
